98c48d88a534b25a60a147b4a841d77205fdd582
rotem711 <rotemskandrani@gmail.com>
2022-06-02 21:58:20 +0200

    add bigquote module

--- components/modules/BigQuote/BigQuote.tsx
+++ components/modules/BigQuote/BigQuote.tsx
@@ -3,12 +3,21 @@ import styles from './BigQuote.module.scss'
 import IBigQuote from './BigQuote.interface'
 
 const BigQuoteModule:FunctionComponent<IBigQuote> = (props) => {
-  console.log(props)
+  const { bigQuote } = props
+
   return (
     <div
-      className={`${styles.root} container`}
+      className={`${styles.root}`}
     >
-      Bigquote Module
+      <div className="container pt-25 pb-155 xl:pt-35 xl:pb-230">
+        <div className="xl:default-grid">
+          <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85`}>{bigQuote.leftHeadline}</h2>
+          <blockquote className={`${styles.quote} typo-big-quotes`}>
+            {bigQuote.quote}
+            <cite className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}>{bigQuote.subline}</cite>
+          </blockquote>
+        </div>
+      </div>
     </div>
   )
 }
