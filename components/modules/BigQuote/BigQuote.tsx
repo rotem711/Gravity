5e50c65f433d9b983a4f0c3f84dd90f0d06a18d9
rotem711 <rotemskandrani@gmail.com>
2022-06-03 14:53:31 +0200

    change quotes binding

--- components/modules/BigQuote/BigQuote.tsx
+++ components/modules/BigQuote/BigQuote.tsx
@@ -8,15 +8,18 @@ const BigQuoteModule:FunctionComponent<IBigQuote> = (props) => {
   return (
     <div
       className={`${styles.root}`}
-      style={{backgroundColor: bigQuote.backgroundColor}}
+      style={{ backgroundColor: bigQuote.backgroundColor }}
     >
       <div className="container pt-25 pb-155 xl:pt-35 xl:pb-230">
         <div className="xl:default-grid">
           <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85`}>{bigQuote.leftHeadline}</h2>
-          <blockquote className={`${styles.quote} typo-big-quotes`}>
-            {bigQuote.quote}
-            <cite className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}>{bigQuote.subline}</cite>
-          </blockquote>
+          {bigQuote.quotes.map((item) => (
+            <blockquote className={`${styles.quote} typo-big-quotes`}>
+              {item.quote}
+              <i aria-hidden="true">”</i>
+              <cite className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}>{item.subline}</cite>
+            </blockquote>
+          ))}
         </div>
       </div>
     </div>
