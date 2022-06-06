b9d549313660a2903bd24df365df047bea405425
rotem711 <rotemskandrani@gmail.com>
2022-06-06 16:49:58 +0200

    start SliderWithMedia

--- components/modules/SliderWithMedia/SliderWithMedia.tsx
+++ components/modules/SliderWithMedia/SliderWithMedia.tsx
@@ -1,24 +1,50 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Button from 'components/generic/button/button'
 import styles from './SliderWithMedia.module.scss'
 import ISliderWithMedia from './SliderWithMedia.interface'
 
 const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
   const { sliderWithMedia } = props
+  const [index, setIndex] = useState(0)
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: false,
+  })
+
+  const onClickItem = (e) => {
+    setIndex(e.currentTarget.dataset.index)
+  }
+
+  useEffect(() => {
+    if (inView) {
+      setTimeout(() => {
+        // if(index < )
+        // return index
+      }, 1000)
+    }
+  }, [inView, index])
 
   return (
     <div
       className={`${styles.root} container pt-35 pb-150`}
+      ref={ref}
     >
       <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85`}>{sliderWithMedia.subline}</h2>
       <ul>
-        {sliderWithMedia.slides.map((item) => (
-          <li className="typo-big-quotes">{item.title}</li>
+        {sliderWithMedia.slides.map((item, itemIndex) => (
+          <li
+            className={`${styles.navItem} ${index == itemIndex ? styles.isActive : ''} typo-big-quotes`}
+            onClick={onClickItem}
+            data-index={itemIndex}
+          >
+            {item.title}
+          </li>
         ))}
       </ul>
       <div className="copy-container">
         {sliderWithMedia.slides.map((item) => (
-          <div className="typo-body">{item.copy}</div>
+          <button type="button" className={` ${index === 0 ? 'is-active' : ''} typo-body`} onClick={onClickItem}>{item.copy}</button>
         ))}
       </div>
       <div className="media-container">
@@ -33,7 +59,7 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
           </div>
         ))}
       </div>
-      <Button variant="light" link={sliderWithMedia.link}/>
+      <Button variant="light" link={sliderWithMedia.link} />
     </div>
   )
 }
