75abb397f0afc07c188d54f8761063418b8cc9f3
rotem711 <rotemskandrani@gmail.com>
2022-06-07 22:10:27 +0200

    animate sliderWithMedia module

--- components/modules/SliderWithMedia/SliderWithMedia.tsx
+++ components/modules/SliderWithMedia/SliderWithMedia.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react'
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import Button from 'components/generic/button/button'
 import styles from './SliderWithMedia.module.scss'
@@ -7,6 +7,11 @@ import ISliderWithMedia from './SliderWithMedia.interface'
 const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
   const { sliderWithMedia } = props
   const [index, setIndex] = useState(0)
+  const [copyHeight, setCopyHeight] = useState(0)
+  const [mediaHeight, setMediaHeight] = useState(0)
+  const copyRefs = useRef([])
+  const mediaRefs = useRef([])
+
   const { ref, inView } = useInView({
     threshold: 0,
     triggerOnce: false,
@@ -15,6 +20,24 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
   const onClickItem = (e) => {
     setIndex(e.currentTarget.dataset.index)
   }
+
+  // set copyContainer height
+  useEffect(() => {
+    for (let i = 0; i < copyRefs.current.length; i += 1) {
+      if (copyRefs.current[i].clientHeight > mediaHeight) {
+        setMediaHeight(copyRefs.current[i].clientHeight)
+      }
+    }
+  })
+
+  // set mediaContainer height
+  useEffect(() => {
+    for (let i = 0; i < mediaRefs.current.length; i += 1) {
+      if (mediaRefs.current[i].clientHeight > mediaHeight) {
+        setMediaHeight(mediaRefs.current[i].clientHeight)
+      }
+    }
+  })
 
   useEffect(() => {
     if (inView) {
@@ -31,7 +54,7 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
       ref={ref}
     >
       <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85`}>{sliderWithMedia.subline}</h2>
-      <ul>
+      <ul className="">
         {sliderWithMedia.slides.map((item, itemIndex) => (
           <li
             className={`${styles.navItem} ${index == itemIndex ? styles.isActive : ''} typo-big-quotes`}
@@ -42,24 +65,49 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
           </li>
         ))}
       </ul>
-      <div className="copy-container">
-        {sliderWithMedia.slides.map((item) => (
-          <button type="button" className={` ${index === 0 ? 'is-active' : ''} typo-body`} onClick={onClickItem}>{item.copy}</button>
-        ))}
-      </div>
-      <div className="media-container">
-        {sliderWithMedia.slides.map((item) => (
-          <div>
-            {item.vimeoVideoUrl}
-            {/* {item.svg.map((item) => (
-              <div>
-                {item.}
-              </div>
-            ))} */}
+      <div
+        className={`${styles.copyContainer}`}
+        style={{ height: mediaHeight }}
+      >
+        {sliderWithMedia.slides.map((item, itemIndex) => (
+          <div
+            className={`${styles.copyItem} ${index == itemIndex ? styles.isActive : ''} typo-body`}
+            ref={(element) => { copyRefs.current[itemIndex] = element }}
+            role="button"
+          >
+            {item.copy}
           </div>
         ))}
       </div>
-      <Button variant="light" link={sliderWithMedia.link} />
+      <div
+        className={`${styles.mediaContainer}`}
+        style={{ height: mediaHeight }}
+      >
+        {sliderWithMedia.slides.map((item, itemIndex) => (
+          <div
+            className={`${styles.mediaItem} ${index == itemIndex ? styles.isActive : ''}`}
+            ref={(element) => { mediaRefs.current[itemIndex] = element }}
+            role="button"
+          >
+            {
+              item.vimeoVideoUrl
+                ? <video src={item.vimeoVideoUrl} playsInline muted loop autoPlay />
+                : (
+                  <div>image</div>
+                  // <Image
+                  //   src={item.svg.sourceUrl}
+                  //   alt={item.svg.altText}
+                  //   width={item.svg.mediaDetails.width}
+                  //   height={item.svg.mediaDetails.height}
+                  // />
+                )
+            }
+          </div>
+        ))}
+      </div>
+      <div className="w-full mt-50">
+        <Button variant="light" link={sliderWithMedia.link} />
+      </div>
     </div>
   )
 }
