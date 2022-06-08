f69ae97d4fa34dfe35d89e25b6bb3642e86c0b9f
rotem711 <rotemskandrani@gmail.com>
2022-06-08 07:39:20 +0200

    finish sliderWithMedia module

--- components/modules/SliderWithMedia/SliderWithMedia.tsx
+++ components/modules/SliderWithMedia/SliderWithMedia.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
+import React, {
+  FunctionComponent, useEffect, useLayoutEffect, useRef, useState,
+} from 'react'
 import { useInView } from 'react-intersection-observer'
 import Button from 'components/generic/button/button'
+import Image from 'next/image'
 import styles from './SliderWithMedia.module.scss'
 import ISliderWithMedia from './SliderWithMedia.interface'
 
@@ -12,26 +15,31 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
   const copyRefs = useRef([])
   const mediaRefs = useRef([])
 
+  let timer = null
+  const indexRef = useRef(index)
+  indexRef.current = index
+
   const { ref, inView } = useInView({
     threshold: 0,
     triggerOnce: false,
   })
 
   const onClickItem = (e) => {
-    setIndex(e.currentTarget.dataset.index)
+    clearTimeout(timer)
+    setIndex(parseInt(e.currentTarget.dataset.index, 10))
   }
 
   // set copyContainer height
-  useEffect(() => {
+  useLayoutEffect(() => {
     for (let i = 0; i < copyRefs.current.length; i += 1) {
-      if (copyRefs.current[i].clientHeight > mediaHeight) {
-        setMediaHeight(copyRefs.current[i].clientHeight)
+      if (copyRefs.current[i].clientHeight > copyHeight) {
+        setCopyHeight(copyRefs.current[i].clientHeight)
       }
     }
   })
 
   // set mediaContainer height
-  useEffect(() => {
+  useLayoutEffect(() => {
     for (let i = 0; i < mediaRefs.current.length; i += 1) {
       if (mediaRefs.current[i].clientHeight > mediaHeight) {
         setMediaHeight(mediaRefs.current[i].clientHeight)
@@ -41,72 +49,88 @@ const SliderWithMediaModule:FunctionComponent<ISliderWithMedia> = (props) => {
 
   useEffect(() => {
     if (inView) {
-      setTimeout(() => {
-        // if(index < )
-        // return index
-      }, 1000)
+      timer = setTimeout(() => {
+        if (indexRef.current + 1 === mediaRefs.current.length) {
+          setIndex(0)
+        } else { setIndex(indexRef.current + 1) }
+      }, 4000)
+      return () => clearTimeout(timer)
     }
+    return null
   }, [inView, index])
 
   return (
     <div
-      className={`${styles.root} container pt-35 pb-150`}
+      className={`${styles.root} container pt-35 pb-100 md:pt-70xl:pt-35 xl:pb-150`}
       ref={ref}
     >
       <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85`}>{sliderWithMedia.subline}</h2>
-      <ul className="">
-        {sliderWithMedia.slides.map((item, itemIndex) => (
-          <li
-            className={`${styles.navItem} ${index == itemIndex ? styles.isActive : ''} typo-big-quotes`}
-            onClick={onClickItem}
-            data-index={itemIndex}
-          >
-            {item.title}
-          </li>
-        ))}
-      </ul>
-      <div
-        className={`${styles.copyContainer}`}
-        style={{ height: mediaHeight }}
-      >
-        {sliderWithMedia.slides.map((item, itemIndex) => (
-          <div
-            className={`${styles.copyItem} ${index == itemIndex ? styles.isActive : ''} typo-body`}
-            ref={(element) => { copyRefs.current[itemIndex] = element }}
-            role="button"
-          >
-            {item.copy}
+      <div className="xl:default-grid">
+        <header className="md:default-grid xl:block xl:col-span-6 md:mb-90">
+          <ul className="md:col-span-6 xl:w-full sm:mb-55 md:mb-0 xl:mb-95">
+            {sliderWithMedia.slides.map((item, itemIndex) => (
+              <li>
+                <button
+                  className={`${styles.navItem} ${index === itemIndex ? styles.isActive : ''} typo-big-quotes`}
+                  onClick={onClickItem}
+                  onKeyPress={onClickItem}
+                  data-index={itemIndex}
+                  type="button"
+                >
+                  {item.title}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="md:col-span-5 xl:col-span-3">
+            <div
+              className={`${styles.copyContainer} md:col-span-6 mb-55`}
+              style={{ height: copyHeight }}
+            >
+              {sliderWithMedia.slides.map((item, itemIndex) => (
+                <div
+                  className={`${styles.copyItem} ${index === itemIndex ? styles.isActive : ''} typo-body`}
+                  ref={(element) => { copyRefs.current[itemIndex] = element }}
+                >
+                  {item.copy}
+                </div>
+              ))}
+            </div>
+            <div className="w-full mt-50 xl:mt-60 sm:hidden md:block">
+              <Button variant="light" link={sliderWithMedia.link} />
+            </div>
           </div>
-        ))}
-      </div>
-      <div
-        className={`${styles.mediaContainer}`}
-        style={{ height: mediaHeight }}
-      >
-        {sliderWithMedia.slides.map((item, itemIndex) => (
-          <div
-            className={`${styles.mediaItem} ${index == itemIndex ? styles.isActive : ''}`}
-            ref={(element) => { mediaRefs.current[itemIndex] = element }}
-            role="button"
-          >
-            {
-              item.vimeoVideoUrl
-                ? <video src={item.vimeoVideoUrl} playsInline muted loop autoPlay />
-                : (
-                  <div>image</div>
-                  // <Image
-                  //   src={item.svg.sourceUrl}
-                  //   alt={item.svg.altText}
-                  //   width={item.svg.mediaDetails.width}
-                  //   height={item.svg.mediaDetails.height}
-                  // />
-                )
-            }
-          </div>
-        ))}
-      </div>
-      <div className="w-full mt-50">
-        <Button variant="light" link={sliderWithMedia.link} />
+        </header>
+        <div
+          className={`${styles.mediaContainer} xl:col-span-6`}
+          style={{ height: mediaHeight }}
+        >
+          {sliderWithMedia.slides.map((item, itemIndex) => (
+            <div
+              className={`${styles.mediaItem} ${index === itemIndex ? styles.isActive : ''}`}
+              ref={(element) => { mediaRefs.current[itemIndex] = element }}
+              role="button"
+            >
+              {
+                item.vimeoVideoUrl
+                  ? <video src={item.vimeoVideoUrl} playsInline muted loop autoPlay />
+                  : (
+                    item.svg && (
+                      <Image
+                        src={item.svg.sourceUrl}
+                        alt={item.svg.altText}
+                        width={item.svg.mediaDetails.width}
+                        height={item.svg.mediaDetails.height}
+                      />
+                    )
+                  )
+              }
+            </div>
+          ))}
+        </div>
+        <div className="w-full mt-50 md:hidden">
+          <Button variant="light" link={sliderWithMedia.link} />
+        </div>
       </div>
     </div>
   )
