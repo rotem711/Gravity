f79ca25da5bdf581aa55e7f0d5b7569db4293030
rotem711 <rotemskandrani@gmail.com>
2022-06-08 16:33:36 +0200

    add sideBySideMedia module

--- components/modules/SideBySideMedia/SideBySideMedia.tsx
+++ components/modules/SideBySideMedia/SideBySideMedia.tsx
@@ -1,13 +1,46 @@
 import React, { FunctionComponent } from 'react'
+import Link from 'next/link'
+import Image from 'components/generic/image/image'
+import Arrow from 'public/icons/icon-arrow.svg'
 import styles from './SideBySideMedia.module.scss'
 import ISideBySideMedia from './SideBySideMedia.interface'
 
-const SideBySideMediaModule:FunctionComponent<ISideBySideMedia> = () => (
-  <div
-    className={`${styles.root} container`}
-  >
-    Sidebysidemedia Module
-  </div>
-)
+const SideBySideMediaModule:FunctionComponent<ISideBySideMedia> = (props) => {
+  const { sideBySideMedia } = props
+
+  return (
+    <div
+      className={`${styles.root} flex flex-wrap`}
+    >
+      {sideBySideMedia.media.map((item) => (
+        <div
+          className={`${styles.item}`}
+        >
+          <div className={`${styles.itemWrapper}`}>
+            <Link href={item.link.url}>
+              <a>
+                <div>
+                  <p className="typo-headlines mb-30">{item.headline}</p>
+                  <div className={`${styles.iconContainer} `}>
+                    <Arrow />
+                  </div>
+                </div>
+                {
+                item.vimeoVideoUrl
+                  ? <video src={item.vimeoVideoUrl} playsInline muted loop autoPlay />
+                  : (
+                    item.image && (
+                      <Image image={item.image} />
+                    )
+                  )
+                }
+              </a>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default SideBySideMediaModule
