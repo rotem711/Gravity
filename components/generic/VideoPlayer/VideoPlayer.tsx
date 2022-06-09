a3c779b0513515407ee5f330ed9e1b28552c59c0
rotem711 <rotemskandrani@gmail.com>
2022-06-09 20:08:44 +0200

    fix build errors

--- components/generic/VideoPlayer/VideoPlayer.tsx
+++ components/generic/VideoPlayer/VideoPlayer.tsx
@@ -2,14 +2,10 @@ import React, { FunctionComponent } from 'react'
 import styles from './VideoPlayer.module.scss'
 import IVideoPlayer from './VideoPlayer.interface'
 
-const VideoPlayer:FunctionComponent<IVideoPlayer> = (props) => {
-  return (
-    <div
-      className={`${styles.root}`}
-    >
-     :)
-    </div>
-  )
-}
+const VideoPlayer:FunctionComponent<IVideoPlayer> = () => (
+  <div
+    className={`${styles.root}`}
+  />
+)
 
 export default VideoPlayer
