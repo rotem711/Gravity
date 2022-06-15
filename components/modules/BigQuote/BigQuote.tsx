a648e7ad5121f491bf33c3c94e56f1039ddce43c
rotem711 <rotemskandrani@gmail.com>
2022-06-15 16:30:51 +0200

    merge

--- components/modules/BigQuote/BigQuote.tsx
+++ components/modules/BigQuote/BigQuote.tsx
@@ -1,12 +1,16 @@
 import React, {
-  FunctionComponent, useEffect, useLayoutEffect, useRef, useState,
+  FunctionComponent,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
 } from 'react'
 import Image from 'next/image'
 import { useInView } from 'react-intersection-observer'
 import styles from './BigQuote.module.scss'
 import IBigQuote from './BigQuote.interface'
 
-const BigQuoteModule:FunctionComponent<IBigQuote> = (props) => {
+const BigQuoteModule: FunctionComponent<IBigQuote> = (props) => {
   const { bigQuote } = props
   const [index, setIndex] = useState(0)
   const [quoteLayout, setQuoteLayout] = useState('simple')
@@ -45,7 +49,9 @@ const BigQuoteModule:FunctionComponent<IBigQuote> = (props) => {
       timer = setTimeout(() => {
         if (indexRef.current + 1 === quoteRefs.current.length) {
           setIndex(0)
-        } else { setIndex(indexRef.current + 1) }
+        } else {
+          setIndex(indexRef.current + 1)
+        }
       }, 4000)
       return () => clearTimeout(timer)
     }
@@ -59,53 +65,73 @@ const BigQuoteModule:FunctionComponent<IBigQuote> = (props) => {
       ref={ref}
     >
       <div className="container pt-25 pb-155 xl:pt-35 xl:pb-230">
-        {
-          quoteLayout === 'simple' ? (
-            <div className="xl:default-grid">
-              <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85 xl:col-span-3`}>{bigQuote.leftHeadline}</h2>
-              {bigQuote.quotes.map((item) => (
-                <blockquote className={`${styles.quoteElement} typo-big-quotes xl:col-span-8 xl:col-start-5`}>
-                  <div dangerouslySetInnerHTML={{ __html: item.quote }} />
-                  <i aria-hidden="true">”</i>
-                  <cite className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}>
-                    <p>{item.subline}</p>
-                  </cite>
+        {quoteLayout === 'simple' ? (
+          <div className="xl:default-grid">
+            <h2
+              className={`${styles.title} typo-subhead uppercase sm:mb-85 xl:col-span-3`}
+            >
+              {bigQuote.leftHeadline}
+            </h2>
+            {bigQuote.quotes.map((item) => (
+              <blockquote
+                className={`${styles.quoteElement} typo-big-quotes xl:col-span-8 xl:col-start-5`}
+              >
+                <div dangerouslySetInnerHTML={{ __html: item.quote }} />
+                <i aria-hidden="true">”</i>
+                <cite
+                  className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}
+                >
+                  <div dangerouslySetInnerHTML={{ __html: item.subline }} />
+                </cite>
+              </blockquote>
+            ))}
+          </div>
+        ) : (
+          <div className="">
+            <h2
+              className={`${styles.title} typo-subhead uppercase sm:mb-85 w-full`}
+            >
+              {bigQuote.leftHeadline}
+            </h2>
+            <div
+              className={`${styles.quoteContainer}`}
+              style={{ height: quoteHeight }}
+            >
+              {bigQuote.quotes.map((item, itemIndex) => (
+                <blockquote
+                  className={`${styles.quote} ${
+                    index === itemIndex ? styles.isActive : ''
+                  } typo-big-quotes default-grid xl:col-span-12`}
+                  ref={(element) => {
+                    quoteRefs.current[itemIndex] = element
+                  }}
+                >
+                  <div
+                    className={`${styles.quoteElement} col-span-6 md:col-span-9`}
+                  >
+                    <div dangerouslySetInnerHTML={{ __html: item.quote }} />
+                    <i aria-hidden="true">”</i>
+                    <cite
+                      className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}
+                    >
+                      <div dangerouslySetInnerHTML={{ __html: item.subline }} />
+                    </cite>
+                  </div>
+                  <div
+                    className={`${styles.logo} col-span-2 md:col-start-11 mb-50 md:mb-0`}
+                  >
+                    <Image
+                      src={item.logo.sourceUrl}
+                      alt={item.logo.altText}
+                      width={item.logo.mediaDetails.width}
+                      height={item.logo.mediaDetails.height}
+                    />
+                  </div>
                 </blockquote>
               ))}
             </div>
-          ) : (
-            <div className="">
-              <h2 className={`${styles.title} typo-subhead uppercase sm:mb-85 w-full`}>{bigQuote.leftHeadline}</h2>
-              <div
-                className={`${styles.quoteContainer}`}
-                style={{ height: quoteHeight }}
-              >
-                {bigQuote.quotes.map((item, itemIndex) => (
-                  <blockquote
-                    className={`${styles.quote} ${index === itemIndex ? styles.isActive : ''} typo-big-quotes default-grid xl:col-span-12`}
-                    ref={(element) => { quoteRefs.current[itemIndex] = element }}
-                  >
-                    <div className={`${styles.quoteElement} col-span-6 md:col-span-9`}>
-                      <div dangerouslySetInnerHTML={{ __html: item.quote }} />
-                      <i aria-hidden="true">”</i>
-                      <cite className={`${styles.cite} typo-captions-and-buttons mt-35 xl:mt-45`}>
-                        <p>{item.subline}</p>
-                      </cite>
-                    </div>
-                    <div className={`${styles.logo} col-span-2 md:col-start-11 mb-50 md:mb-0`}>
-                      <Image
-                        src={item.logo.sourceUrl}
-                        alt={item.logo.altText}
-                        width={item.logo.mediaDetails.width}
-                        height={item.logo.mediaDetails.height}
-                      />
-                    </div>
-                  </blockquote>
-                ))}
-              </div>
-            </div>
-          )
-        }
+          </div>
+        )}
       </div>
     </div>
   )
