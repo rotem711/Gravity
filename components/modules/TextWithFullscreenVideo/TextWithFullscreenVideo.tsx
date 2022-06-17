1e85ade5a8985860d7d74c3b352471ab1e9b7fbd
rotem711 <rotemskandrani@gmail.com>
2022-06-17 06:48:09 +0200

    fixes

--- components/modules/TextWithFullscreenVideo/TextWithFullscreenVideo.tsx
+++ components/modules/TextWithFullscreenVideo/TextWithFullscreenVideo.tsx
@@ -17,27 +17,16 @@ const TextWithFullscreenVideoModule: FunctionComponent<
           dangerouslySetInnerHTML={{ __html: textWithFullscreenVideo.headline }}
         />
       </Fade>
-      <div className="col-span-6 md:col-span-12 mb-60 md:mb-0 md:mt-75 xl:mt-130 md:order-3 default-grid">
+      <div className="col-span-6 md:col-span-12 mb-60 md:mb-0 md:mt-75 xl:mt-130 md:order-3">
         {textWithFullscreenVideo.vimeoVideoUrl && (
           <video
-            className="col-span-6 md:col-span-8 xl:col-span-7 xl:col-start-2 mb-75 md:mb-0"
+            className="w-full mb-75 md:mb-0"
             src={textWithFullscreenVideo.vimeoVideoUrl}
             playsInline
             muted
             loop
             autoPlay
           />
-        )}
-        {false && (
-          <span className={`${styles.iconContainer} col-span-2 pr-10 md:pr-50`}>
-            <Image
-              layout="responsive"
-              src={textWithFullscreenVideo.logo.sourceUrl}
-              alt={textWithFullscreenVideo.logo.altText}
-              width={textWithFullscreenVideo.logo.mediaDetails.width}
-              height={textWithFullscreenVideo.logo.mediaDetails.height}
-            />
-          </span>
         )}
       </div>
       <div className="col-span-6 md:col-span-5 md:col-start-8 xl:col-span-4 xl:col-start-9 md:order-2">
