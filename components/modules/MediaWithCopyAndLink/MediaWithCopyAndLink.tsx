74151901c5b9ea32207ed36602ec5ad1444a4446
rotem711 <rotemskandrani@gmail.com>
2022-06-17 16:13:55 +0200

    build fix

--- components/modules/MediaWithCopyAndLink/MediaWithCopyAndLink.tsx
+++ components/modules/MediaWithCopyAndLink/MediaWithCopyAndLink.tsx
@@ -14,11 +14,7 @@ const MediaWithCopyAndLinkModule:FunctionComponent<IMediaWithCopyAndLink> = (pro
     >
       <div className="container default-grid pt-30 pb-180 md:pb-200">
         <div className="col-span-6 md:col-span-8 xl:col-span-7 xl:col-start-5">
-          {
-            mediaWithCopyAndLink.vimeoVideoUrl && (
-              <video src={mediaWithCopyAndLink.vimeoVideoUrl} playsInline muted loop autoPlay />
-            )
-          }
+          XXX
         </div>
         <div className="col-span-5 md:col-span-5 xl:col-span-3 xl:col-start-5 xl:-top-75 relative z-1">
           <h2 className="typo-subhead uppercase mb-50"><Fade>{mediaWithCopyAndLink.subline}</Fade></h2>
