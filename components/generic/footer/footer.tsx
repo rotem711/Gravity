879c95375d3fd9b54281f32d666c71bee9acce66
rotem711 <rotemskandrani@gmail.com>
2022-06-21 13:25:49 +0200

    footer update

--- components/generic/footer/footer.tsx
+++ components/generic/footer/footer.tsx
@@ -5,21 +5,21 @@ import FooterInterface from './footer.interface'
 import styles from './footer.module.scss'
 
 const FooterBlock = ({ data }: { data: FooterInterface }) => (
-  <footer
-    className={`${styles.root} container pt-30 pb-45 md:pt-35 md:pb-75 xl:pt-40 xl:pb-45`}
-  >
+  <footer className={`${styles.root} container pt-30 pb-45 md:pt-40 md:pb-45`}>
     <Link href="/">
-      <a><SimpleLogo className={`${styles.logo} mb-100 md:mb-110 xl:mb-125`} /></a>
+      <a>
+        <SimpleLogo className={`${styles.logo} mb-100 md:mb-125`} />
+      </a>
     </Link>
     <div className="default-grid">
       <div
         className={`${styles.contact} col-span-6 md:col-span-5 typo-captions-and-buttons`}
         dangerouslySetInnerHTML={{ __html: data.contact }}
       />
-      <ul className="col-span-6 md:col-span-12 xl:col-span-6 xl:col-start-7 mt-150 md:mt-100 xl:mt-0 default-grid typo-captions-and-buttons">
+      <ul className="col-span-6  md:col-span-6 md:col-start-7 mt-150  md:mt-0 default-grid typo-captions-and-buttons">
         {data.navigation.map((item) => (
           <li
-            className="col-span-3 xl:col-span-4 mb-75 last:mb-0 md:mb-0"
+            className="col-span-3 md:col-span-4 mb-75 last:mb-0 md:mb-0"
             key={item.label}
           >
             <span>{item.label}</span>
