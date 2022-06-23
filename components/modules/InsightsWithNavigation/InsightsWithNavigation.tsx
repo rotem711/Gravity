8ce5d4985351b488bb183471679c1dd6de0ca208
rotem711 <rotemskandrani@gmail.com>
2022-06-23 09:36:16 +0200

    add module

--- components/modules/InsightsWithNavigation/InsightsWithNavigation.tsx
+++ components/modules/InsightsWithNavigation/InsightsWithNavigation.tsx
@@ -1,11 +1,12 @@
 import { GlobalContext } from 'pages/_app'
 import React, { useContext } from 'react'
+import IInsightsWithNavigation from './InsightsWithNavigation.interface'
 import styles from './InsightsWithNavigation.module.scss'
 
-const InsightsWithNavigationModule = () => {
+const InsightsWithNavigationModule = ({ textVideoCombinationV2 }: IInsightsWithNavigation) => {
   const ctx = useContext(GlobalContext)
   const { insights, insightsCategories } = ctx
-  console.log(insights, insightsCategories)
+  console.log(insights, insightsCategories, textVideoCombinationV2)
   return (
     <div className={`${styles.root}`}>
       <div className="container">Insightswithnavigation Module</div>
