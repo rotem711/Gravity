046c8e07f1d54c5afdc8b8a5f76ad31de8099309
rotem711 <rotemskandrani@gmail.com>
2022-07-07 09:06:26 +0200

    fixes

--- components/modules/MediaWithCopyAndLink/MediaWithCopyAndLink.tsx
+++ components/modules/MediaWithCopyAndLink/MediaWithCopyAndLink.tsx
@@ -14,12 +14,12 @@ const MediaWithCopyAndLinkModule:FunctionComponent<IMediaWithCopyAndLink> = (pro
       style={{ backgroundColor: mediaWithCopyAndLink.backgroundColor }}
     >
       <div className="container default-grid pt-30 pb-180 md:pb-200">
-        <div className="col-span-6 md:col-span-8 xl:col-span-8 xl:col-start-5">
+        <div className="col-span-6 md:col-span-8 md:col-start-5">
           <LottiePlayer
             animation={mediaWithCopyAndLink.lottieSelect}
           />
         </div>
-        <div className="col-span-6 md:col-span-5 xl:col-span-3 xl:col-start-5 -top-40 md:-top-30 xl:-top-100 relative z-1">
+        <div className="col-span-6 md:col-span-5 xl:col-span-3 md:col-start-5 -top-40 md:-top-100 relative z-1">
           <h2 className="typo-subhead uppercase mb-50"><Fade>{mediaWithCopyAndLink.subline}</Fade></h2>
           <div className="typo-body mb-50"><Fade delay={150}>{mediaWithCopyAndLink.headline}</Fade></div>
           <Fade delay={300}><Button variant="light" link={mediaWithCopyAndLink.link} /></Fade>
