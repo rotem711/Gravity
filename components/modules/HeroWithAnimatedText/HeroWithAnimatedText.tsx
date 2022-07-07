93b47fa66bd95fb88c4f233e067d375246435c95
rotem711 <rotemskandrani@gmail.com>
2022-07-07 12:26:04 +0200

    fix: HeroWithAnimatedText animation

--- components/modules/HeroWithAnimatedText/HeroWithAnimatedText.tsx
+++ components/modules/HeroWithAnimatedText/HeroWithAnimatedText.tsx
@@ -12,80 +12,65 @@ const HeroWithAnimatedTextModule: FunctionComponent<IHeroWithAnimatedText> = (
   props,
 ) => {
   const { heroWithAnimatedText } = props
-
   const refs = useRef<HTMLSpanElement[]>([])
+  const lastLineRef = useRef<HTMLDivElement>()
+  const spacerRef = useRef<HTMLDivElement>()
 
   useEffect(() => {
     ScrollTrigger.create({
       trigger: '#team-header',
       start: 'top top',
-      end: 'bottom+=300% top',
-      pin: true,
-      pinSpacing: true,
+      end: 'bottom bottom',
     })
-    refs.current.forEach(() => {
-      gsap
-        .timeline({
-          duration: 40,
-          scrollTrigger: {
-            trigger: '#team-header',
-            start: 'top top',
-            end: 'bottom+=300% bottom',
-            pin: true,
-            scrub: 1,
-            pinSpacing: true,
-          },
-        })
-        .to('.line', {
-          opacity: 1,
-          duration: 6,
-          stagger: 6,
-          delay: -40,
-        })
-        .to('.list', {
-          y: '-35%',
-          delay: -6,
-          duration: 8,
-        })
-        .to('.items', {
-          opacity: 0,
-          duration: 6,
-          delay: -6,
-        })
-    })
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: spacerRef.current,
+          start: 'top bottom',
+          end: 'bottom bottom',
+          scrub: 1,
+        },
+      })
+      .fromTo([...refs.current, lastLineRef.current], { opacity: 0 }, { opacity: 1, stagger: 0.2 })
   }, [refs])
 
   return (
     <div
       id="team-header"
-      className={`${styles.root} pt-180 lg:pt-225 pb-150 lg:pb-190`}
+      className={`${styles.root}`}
     >
-      <div className="container">
-        <div className={`${styles.list} list`}>
-          <div className={`${styles.items} items`}>
-            {heroWithAnimatedText.animatingText.map((item) => (
-              <div
-                className={`${styles.line} line`}
-                ref={(r) => refs.current.push(r)}
-                key={item.text}
-              >
-                {item.text}
-              </div>
-            ))}
+      <div
+        className={`${styles.pin} pt-180 lg:pt-225 pb-150 lg:pb-190`}
+      >
+        <div className="container">
+          <div className={`${styles.list} list`}>
+            <div className={`${styles.items} items`}>
+              {heroWithAnimatedText.animatingText.map((item) => (
+                <div
+                  className={`${styles.line} line`}
+                  ref={(r) => refs.current.push(r)}
+                  key={item.text}
+                >
+                  {item.text}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div ref={lastLineRef} className={`relative ${styles.line} line last mt-150 lg:mt-230 z-[1]`}>
+            {heroWithAnimatedText.subline}
           </div>
         </div>
-        <div className={`relative ${styles.line} line last mt-150 lg:mt-230 z-[1]`}>
-          {heroWithAnimatedText.subline}
-        </div>
+        <video
+          className={`${styles.video}`}
+          src={heroWithAnimatedText.vimeoVideoUrl}
+          playsInline
+          muted
+          loop
+          autoPlay
+        />
       </div>
-      <video
-        className={`${styles.video}`}
-        src={heroWithAnimatedText.vimeoVideoUrl}
-        playsInline
-        muted
-        loop
-        autoPlay
-      />
+      <div ref={spacerRef} className={styles.spacer} />
     </div>
   )
 }
