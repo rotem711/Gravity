02907d812a5c700cce8fd10b1d57e572c8d7294a
rotem711 <rotemskandrani@gmail.com>
2022-07-09 16:21:57 +0200

    fixes

--- components/modules/TextVideoCombination/TextVideoCombination.tsx
+++ components/modules/TextVideoCombination/TextVideoCombination.tsx
@@ -21,32 +21,39 @@ const TextVideoCombinationModule: FunctionComponent<ITextVideoCombination> = (
     <div ref={ref} className={`${styles.root}`}>
       <div className="container">
         {textVideoCombination.items.map((item) => (
-          <div key={item.headline} className={`${styles.item} md:default-grid`}>
-            <div className="md:col-span-6 xl:col-span-4 flex flex-col md:row-start-2">
+          <div key={item.headline} className={`${styles.item} ms:default-grid`}>
+            {item.topHeadline && (
+              <span
+                className={`col-span-full typo-subhead uppercase mb-100 hidden ms:block md:hidden ${styles.topHeadline}`}
+              >
+                <Fade>{item.topHeadline}</Fade>
+              </span>
+            )}
+            <div className="flex ms:col-span-3 md:col-span-6 xl:col-span-4 flex-col md:row-start-2">
               {item.topHeadline && (
                 <span
-                  className={`typo-subhead uppercase mb-90 md:mb-75 block ${styles.topHeadline}`}
+                  className={`ms:hidden typo-subhead uppercase mb-90 md:mb-75 md:block ${styles.topHeadline}`}
                 >
                   <Fade>{item.topHeadline}</Fade>
                 </span>
               )}
-              <h2 className="typo-headlines mb-50 md:mb-180">
+              <h2 className="typo-headlines mb-50 sd:mb-125 md:mb-180">
                 <Fade delay={200}>{item.headline}</Fade>
               </h2>
               <Fade delay={300}>
                 <div
-                  className="typo-body mb-45 hidden md:block md:mb-60"
+                  className="typo-body mb-45 hidden ms:block md:mb-60"
                   dangerouslySetInnerHTML={{ __html: item.copy }}
                 />
               </Fade>
-              <div className="w-full mt-50 md:mt-0 hidden md:block">
+              <div className="w-full mt-50 sd:mt-0 hidden ms:block">
                 <Fade delay={400}>
                   <Button variant="light" link={item.link} />
                 </Fade>
               </div>
             </div>
             <div
-              className={`${styles.mediaItem} col-span-6 md:col-span-5 md:col-start-8 md:row-start-2`}
+              className={`${styles.mediaItem} col-span-6 ms:col-span-3 md:col-span-5 md:col-start-8 md:row-start-2`}
             >
               {item.image.desktopImage || item.image.mobileImage ? (
                 item.image && <Image image={item.image} />
@@ -66,11 +73,11 @@ const TextVideoCombinationModule: FunctionComponent<ITextVideoCombination> = (
             </div>
             <Fade delay={300}>
               <div
-                className="typo-body mb-45 mt-50 md:hidden"
+                className={`typo-body mb-45 mt-50 ms:hidden ${styles.copy}`}
                 dangerouslySetInnerHTML={{ __html: item.copy }}
               />
             </Fade>
-            <div className="w-full md:hidden">
+            <div className="w-full ms:hidden">
               <Button variant="light" link={item.link} />
             </div>
           </div>
