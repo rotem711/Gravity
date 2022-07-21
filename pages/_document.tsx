8a6c82de970ca3fd035feb1ce76d900809dccc68
rotem711 <rotemskandrani@gmail.com>
2022-07-21 21:07:06 +0200

    removed usersnap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -64,21 +64,6 @@ class MyDocument extends Document {
               }}
             />
           )}
-          {process.env.NODE_ENV !== 'development' && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-            window.onUsersnapCXLoad = function(api) {
-              api.init();
-            }
-            var script = document.createElement('script');
-            script.defer = 1;
-            script.src = 'https://widget.usersnap.com/global/load/41370aae-428e-4105-8a41-f685a901be75?onload=onUsersnapCXLoad';
-            document.getElementsByTagName('head')[0].appendChild(script);
-            `,
-              }}
-            />
-          )}
         </body>
       </Html>
     )
