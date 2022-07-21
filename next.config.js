a85aaae6190aed0a74305267bf23616832b33ada
rotem711 <rotemskandrani@gmail.com>
2022-07-21 22:55:00 +0200

    csp

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' player.vimeo.com 'self' www.google-analytics.com vimeo.com",
+            value: "default-src 'self' player.vimeo.com;connect-src api.hsforms.com 'self' www.google-analytics.com vimeo.com; www.google-analytics.com 'self' data:; script-src 'unsafe-eval' player.vimeo.com www.googletagmanager.com www.google-analytics.com 'unsafe-inline' 'self'; script-src-attr 'unsafe-inline'; font-src 'self' data:; frame-src player.vimeo.com",
           },
           {
             key: 'X-Frame-Options',
