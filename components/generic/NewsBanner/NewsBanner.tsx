9c26ca40e75220d4b8ae01f33ecbf113bc75718f
rotem711 <rotemskandrani@gmail.com>
2022-07-21 08:24:30 +0200

    fix

--- components/generic/NewsBanner/NewsBanner.tsx
+++ components/generic/NewsBanner/NewsBanner.tsx
@@ -13,18 +13,22 @@ const NewsBanner: FunctionComponent<INewsBanner> = ({ data, onClose }) => {
   }
   return (
     <div className={`${styles.root} ${closed ? styles.hide : ''}`}>
-      <Link href={data.newsBannerLink.url}>
-        <a target={data.newsBannerLink.target}>
-          <div className="container flex items-center justify-center h-full">
+      <div className="container flex items-center justify-center h-full">
+        <Link href={data.newsBannerLink.url}>
+          <a
+            className="flex items-center justify-center"
+            target={data.newsBannerLink.target}
+          >
             <div className="relative text-center px-20 md:px-0">
               <span>{data.newsBannerText}</span>
             </div>
             <div className={styles.chevron}>
               <ArrowRight />
             </div>
-          </div>
-        </a>
-      </Link>
+          </a>
+        </Link>
+      </div>
+
       <button type="button" onClick={onCloseAction}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 13 13">
           <path stroke="#F7F7F2" d="m1 1 11 11M1 12 12 1" />
