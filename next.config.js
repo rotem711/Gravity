b4b33f908e30ec17fb73fb1d96453d208c3b313b
rotem711 <rotemskandrani@gmail.com>
2022-07-21 22:48:10 +0200

    csp headers

--- next.config.js
+++ next.config.js
@@ -2,16 +2,18 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          svgo: {
-            plugins: {
-              removeViewBox: false,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: {
+              plugins: {
+                removeViewBox: false,
+              },
             },
           },
         },
-      }],
+      ],
     })
 
     return config
@@ -31,5 +33,38 @@ module.exports = {
   images: {
     domains: [`${process.env.NEXT_PUBLIC_ASSET_DOMAIN}`, 'secure.gravatar.com'],
     formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' player.vimeo.com 'self' vimeo.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(); battery=(self); geolocation=(); microphone=();',
+          },
+        ],
+      },
+    ]
   },
 }
