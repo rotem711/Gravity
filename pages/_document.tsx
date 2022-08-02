9e6d28eee06e1e0391f4f80d4461e3e4ae1e4092
rotem711 <rotemskandrani@gmail.com>
2022-08-02 19:00:03 +0200

    build fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, {
+  Head, Html, Main, NextScript,
+} from 'next/document'
 
 class MyDocument extends Document {
   render() {
