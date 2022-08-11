10821892c2e62c720de93ddb6e1cc7d8ced4b2fe
rotem711 <rotemskandrani@gmail.com>
2022-08-11 08:19:39 +0200

    csp

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' script.hotjar.com ws.zoominfo.com static.hotjar.com *.googletagmanager.com vimeo.com player.vimeo.com; img-src 'self' data: *.google-analytics.com aorta.clickagy.com *.googletagmanager.com; connect-src 'self' *.google-analytics.com api.hsforms.com static.hotjar.com in.hotjar.com script.hotjar.com ws.zoominfo.com *.analytics.google.com *.googletagmanager.com",
+            value: "script-src 'self' 'unsafe-inline' ws37.hotjar.com script.hotjar.com ws.zoominfo.com static.hotjar.com *.googletagmanager.com vimeo.com player.vimeo.com; img-src 'self' data: *.google-analytics.com aorta.clickagy.com pixel-sync.sitescout.com *.googletagmanager.com; connect-src 'self' *.google-analytics.com api.hsforms.com static.hotjar.com in.hotjar.com script.hotjar.com ws.zoominfo.com *.analytics.google.com *.googletagmanager.com",
           },
           {
             key: 'X-Frame-Options',
