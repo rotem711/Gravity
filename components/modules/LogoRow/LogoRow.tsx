bb75298aae32d45d07cdeb75c52cd38f2ab4c1ef
rotem711 <rotemskandrani@gmail.com>
2022-08-24 18:23:13 +0200

    hotfix

--- components/modules/LogoRow/LogoRow.tsx
+++ components/modules/LogoRow/LogoRow.tsx
@@ -2,10 +2,5 @@ import React, { FunctionComponent } from 'react'
 import styles from './LogoRow.module.scss'
 import ILogoRow from './LogoRow.interface'
 
-const LogoRowModule: FunctionComponent<ILogoRow> = () => (
-  <div className={`${styles.root}`}>
-    <div className="container">Logorow Module</div>
-  </div>
-)
-
+const LogoRowModule: FunctionComponent<ILogoRow> = () => <div className={`${styles.root} hidden`} />
 export default LogoRowModule
