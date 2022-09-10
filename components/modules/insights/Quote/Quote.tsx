b20a806749b188c4aa72ef4de23747edb09b784c
rotem711 <rotemskandrani@gmail.com>
2022-09-10 10:00:20 -0700

    update quote module

--- components/modules/insights/Quote/Quote.tsx
+++ components/modules/insights/Quote/Quote.tsx
@@ -1,35 +1,20 @@
 import React, { FunctionComponent } from 'react'
 import parse from 'html-react-parser'
-import Link from 'next/link'
 import styles from './Quote.module.scss'
 import IQuote from './Quote.interface'
 
-const QuoteModule: FunctionComponent<IQuote> = ({
-  quote,
-  author,
-  anchorLink,
-}) => (
+const QuoteModule: FunctionComponent<IQuote> = ({ quote, author }) => (
   <div className={`${styles.root} mb-70`}>
     <blockquote className="container-insights">
-      {console.log(anchorLink, author)}
-      {anchorLink && (
-        <Link href={anchorLink}>
-          <a className="typo-headlines relative">
-            <span className={styles.quote_mark}>“</span>
-            {parse(quote)}
-            <span>”</span>
-          </a>
-        </Link>
-      )}
-      {!anchorLink && (
-        <p className="typo-headlines relative">
-          <span className={styles.quote_mark}>“</span>
-            {parse(quote)}
-          <span>”</span>
-        </p>
-      )}
+      <p className="typo-headlines relative">
+        <span className={styles.quote_mark}>“</span>
+        {parse(quote)}
+        <span>”</span>
+      </p>
       {author && (
-        <cite className="block typo-captions-and-buttons mt-30">
+        <cite
+          className={`block typo-captions-and-buttons mt-30 ${styles.author}`}
+        >
           {parse(author)}
         </cite>
       )}
